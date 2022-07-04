adb8a7eb7a58aa16c9a16bfe59ab6063f4b72e26
idea <linhao997@qq.com>
2022-07-04 10:09:52 +0800

    cow案例

---
+++ src/main/java/并发编程09/copyonwrite模式/CopyOnWriteDemo.java
@@ -0,0 +1,33 @@
+package 并发编程09.copyonwrite模式;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+/**
+ * @Author linhao
+ * @Date created in 10:42 上午 2022/7/3
+ */
+public class CopyOnWriteDemo {
+
+    @Test
+    public void showCopyOnWriteArrayList() {
+        CopyOnWriteArrayList<String> copyOnWriteArrayList = new CopyOnWriteArrayList();
+        copyOnWriteArrayList.add("idea1");
+        copyOnWriteArrayList.add("idea2");
+        copyOnWriteArrayList.add("idea3");
+        copyOnWriteArrayList.add("idea4");
+        System.out.println(copyOnWriteArrayList.get(0));
+    }
+
+    @Test
+    public void showCopyOnWriteArraySet() {
+        CopyOnWriteArraySet copyOnWriteArraySet = new CopyOnWriteArraySet();
+        copyOnWriteArraySet.add("t1");
+        copyOnWriteArraySet.add("t1");
+        copyOnWriteArraySet.add("t1");
+        System.out.println(copyOnWriteArraySet.size());
+    }
+
+}
